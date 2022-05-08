64be42902117dbf40d6ab01357858c6e2b5ece23
Phillip DuLion <phillip@dulion.com>
2022-05-08 14:11:31 -0700

    Boilerplate

---
+++ src/main/java/com/dulion/green/pi/PiApplication.java
@@ -0,0 +1,13 @@
+package com.dulion.green.pi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiApplication.class, args);
+	}
+
+}
